f8d1bc3fa48a9bb79ede6c8cfca86ffcd5e336c7
data22 <lyebh1234@gmail.com>
2023-03-24 15:32:43 +0900

    second

---
+++ src/main/java/Q_1000.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int result;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        result = a + b;
+        System.out.printf("%d", result);
+    }
+}
+
+
+
+
+
