65a61dd569278bc86c4736c7f1009c42e82ba545
data22 <lyebh1234@gmail.com>
2023-03-24 16:22:16 +0900

    second

--- src/main/java/Q_1008.java
+++ src/main/java/Q_1008.java
@@ -4,12 +4,12 @@ public class Q_1008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
-        int b;
+        double b;
         a = sc.nextInt();
         b = sc.nextInt();
 
-        int result;
-        result = a*b;
-        System.out.printf("%d", result);
+        double result;
+        result = a/b;
+        System.out.printf("%17.16f", result);
     }
 }
