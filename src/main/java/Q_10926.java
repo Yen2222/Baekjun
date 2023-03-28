d7b3c10ea38b03f88715d831daa3250ae4d1bca5
data22 <lyebh1234@gmail.com>
2023-03-28 17:41:56 +0900

    second

---
+++ src/main/java/Q_10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = "joonas";
+        str1 = sc.next();
+
+        System.out.printf("%s??! \n", str1);
+    }
+}
