2931a81672588f7289496157df5bb272260459f4
data22 <lyebh1234@gmail.com>
2023-04-03 19:22:16 +0900

    second

--- src/main/java/Q_10430.java
+++ src/main/java/Q_10430.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Q_10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A,B,C;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+
+        System.out.printf("%d \n", (A+B)%C);
+        System.out.printf("%d \n", ((A%C)+(B%C))%C);
+        System.out.printf("%d \n", (A*B)%C);
+        System.out.printf("%d \n", ((A%C)*(B%C))%C);
+    }
 }
