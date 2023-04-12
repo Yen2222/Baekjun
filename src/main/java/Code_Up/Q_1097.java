21b8bfa0ba9f7ed5ffe16baf5e8fe84861ee12b7
data22 <lyebh1234@gmail.com>
2023-04-12 15:00:06 +0900

    2023.04.12 이예은 코딩

---
+++ src/main/java/Code_Up/Q_1097.java
@@ -0,0 +1,37 @@
+package Code_Up;
+
+import java.util.Scanner;
+
+public class Q_1097 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int aa [][] = new int[19][19];
+        int bb [] = new int[2];
+        int cc [] = new int[2];
+
+        for(int i = 0; i<aa.length; i++){
+            for(int j = 0; j<aa.length; j++){
+               aa[i][j] = sc.nextInt();
+            }
+        }
+
+        int n = sc.nextInt();
+        for(int i =0; i<n; i++){
+            bb[i] = sc.nextInt();
+            cc[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<aa.length; i++){
+            for(int j = 0; j<aa.length; j++){
+                if(i== bb[0]-1 || i == bb[1]-1 || j== cc[0]-1 || j == cc[1]-1){
+                    aa [i][j] --;
+                    if(i==bb[0]-1&&j== cc[0]-1 || i == bb[1]-1&&j== cc[0]-1 || i== bb[0]-1&&j == cc[1]-1 || i == bb[1]-1&&j == cc[1]-1)
+                        aa[i][j]++;
+                }
+                System.out.printf("%d ", aa[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
