bf11391601fb6e305b7436787c57b45a496fe71d
data22 <lyebh1234@gmail.com>
2023-05-08 12:44:36 +0900

    2023.05.05 이예은 코딩

---
+++ src/main/java/Test_major/Q_2743.java
@@ -0,0 +1,14 @@
+package Test_major;
+
+import java.util.Scanner;
+
+public class Q_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+        int wolen = word.length();
+
+        System.out.println(wolen);
+    }
+}
---
+++ src/main/java/Test_major/Q_9086.java
@@ -0,0 +1,17 @@
+package Test_major;
+
+import java.util.Scanner;
+
+public class Q_9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++){
+            String st = sc.nextLine();
+            int str = st.length();
+            System.out.print(str);
+        }
+    }
+}
