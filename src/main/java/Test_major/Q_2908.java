c91644916c996da88524d1adb01f851f6d43ee52
data22 <lyebh1234@gmail.com>
2023-05-15 19:31:54 +0900

    2023.05.15 이예은 코딩

--- src/main/java/Test_major/Q_2908.java
+++ src/main/java/Test_major/Q_2908.java
@@ -6,49 +6,18 @@ public class Q_2908 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       String A,B;
-       while (true){
-             A = sc.next();
-             for (int i = 0; i < 3; i++) {
-                 if (A.charAt(i) == 0) {
-                     A = sc.next();
-                     i=0;
-                 }
-                 else if(A.charAt(2) != 0)
-                     break;
-             }
-           B = sc.next();
-           for (int i = 0; i < 3; i++) {
-               if (B.charAt(i) == 0) {
-                   B = sc.next();
-                   i=0;
-               }
-               if(A.equals(B)) {
-                   B = sc.next();
-                   i=0;
-               }
-               else if(B.charAt(2) != 0)
-                   break;
-           }
-           break;
-       }
+        String A, B;
+        A = sc.next();
+        B = sc.next();
+        String a [] = A.split("");
+        String b [] = B.split("");
 
-       if (A.charAt(2) > B.charAt(2)) {
-           for (int i = 3; i > 0; i--) {
-               System.out.print(A.charAt(i - 1));
-           }
-       } else if(A.charAt(1) > B.charAt(1)){
-           for (int i = 3; i > 0; i--) {
-               System.out.print(A.charAt(i - 1));
-           }
-       } else if(A.charAt(0) > B.charAt(0)){
-           for (int i = 3; i > 0; i--) {
-               System.out.print(A.charAt(i - 1));
-           }
-       } else {
-           for (int i = 3; i > 0; i--) {
-               System.out.print(B.charAt(i - 1));
-           }
-       }
+        String aa = (a[2]+a[1]+a[0]);
+        String bb = (b[2]+b[1]+b[0]);
+
+        if(Integer.parseInt(aa)<Integer.parseInt(bb))
+            System.out.println(bb);
+        else
+            System.out.println(aa);
     }
 }
---
+++ src/main/java/Test_major/Q_5622.java
@@ -0,0 +1,7 @@
+package Test_major;
+
+public class Q_5622 {
+    public static void main(String[] args) {
+
+    }
+}
