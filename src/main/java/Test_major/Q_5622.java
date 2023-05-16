d7813653eafdf2efe8eb2f88157f36d0a5f74c12
data22 <lyebh1234@gmail.com>
2023-05-16 11:04:01 +0900

    2023.05.15 이예은 코딩

--- src/main/java/Test_major/Q_5622.java
+++ src/main/java/Test_major/Q_5622.java
@@ -1,7 +1,44 @@
 package Test_major;
 
+import java.util.Scanner;
+
 public class Q_5622 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String st = sc.next();
+        String ss [] = st.split("");
+//        int al [] = new int[ss.length];
+//        int a = 65;
+        int count=0;
+        int b=3;
+        int c =0;
+        int d = 0;
+
+//        for (int i=0; i<26; i++){
+//            al[i] = a;
+//            a++;
+//        }
+        for(int i = 0; i< ss.length; i++){
+               for(int j=0; j<3; j++){
+                   if((int)ss[i].charAt(0)-65 == 18 ) {
+                       count += 8;
+                       c++;
+                       j=0;
+                   } else if ((int)ss[i].charAt(0)-65 == 25) {
+                       count+=10;
+                       break;
+                   }
+                   else if ((int)ss[i].charAt(0)-65 == c){
+                       count += 3+d;
+                       c++;
+                       if(j==2) {
+                           j = 0;
+                           d++;
+                       }
+                    }
+               }
+        }
+        System.out.println(count);
     }
 }
